37b5d1056e43748da5870fe88edaba37e46df1cd
s0rabh7 <sourabhgawande713@gmail.com>
2025-11-27 23:44:29 +0530

    initialize backend

---
+++ backend/controller/userController.js
@@ -0,0 +1,17 @@
+
+// router for user login
+const loginUser = async(req, res) => {
+
+}
+
+// route for user registration
+const registerUser = async(req, res) => {
+    res.json("register api working")
+}
+
+// route for admin login
+const adminLogin = async(req, res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin}
